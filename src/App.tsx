d420eb1acd1da82c04aa15a6e4aaddb405921cde
matheus_alexandria <maalexandria@hotmail.com>
2026-01-17 16:48:26 -0300

    fix: remove react router dom to test gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ItemForm from './components/ItemForm';
-import PrintPage from './components/PrintPage';
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ItemForm />} />
-        <Route path="/print" element={<PrintPage />} />
-      </Routes>
-    </Router>
+    <ItemForm />
   );
 }
